e47358e6a9e01c1568f0e05c68c995f0d8049739
sumi-03 <100473912+sumi-03@users.noreply.github.com>
2024-08-30 16:05:04 +0900

    [level 1] Title: 카드 뭉치, Time: 0.21 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class Solution {
+
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+
+        Stack<String> stack1 = new Stack<String>();
+        Stack<String> stack2 = new Stack<String>();
+        String answer = "Yes";
+
+        for (int i = cards1.length - 1; i >= 0; i--) {
+
+            stack1.push(cards1[i]);
+        }
+
+        for (int i = cards2.length - 1; i >= 0; i--) {
+
+            stack2.push(cards2[i]);
+        }
+
+        for (int i = 0; i < goal.length; i++) {
+
+            if (!stack1.isEmpty() && goal[i].equals(stack1.peek())) {
+
+                stack1.pop();
+            } 
+            else if (!stack2.isEmpty() && goal[i].equals(stack2.peek())) {
+
+                stack2.pop();
+            } 
+            else {
+                
+                answer = "No";
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
