042e6ae321cc83bdcd1d55f1fe8ff97549f3c009
sumi-03 <lsmkoo03@naver.com>
2025-02-13 19:05:57 +0900

    [level 1] Title: 신규 아이디 추천, Time: 18.48 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(String new_id) {
+
+        String answer = new_id.toLowerCase();
+
+        answer = answer.replaceAll("[^a-z0-9-_.]", "");
+        answer = answer.replaceAll("\\.{2,}", ".");
+        answer = answer.replaceAll("^\\.|\\.$", "");
+
+        if (answer.isEmpty()) {
+
+            answer = "a";
+        }
+
+        if (answer.length() >= 16) {
+
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("\\.$", "");
+        }
+
+        while (answer.length() < 3) {
+
+            answer += answer.charAt(answer.length() - 1);
+        }
+
+        return answer;
+    }
+}
