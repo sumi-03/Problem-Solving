8215b00da36f44b98c1dd4ee31b4ec935dbd5e87
sumi-03 <lsmkoo03@naver.com>
2026-08-31 12:36:37 +0900

    [level 3] Title: 입국심사, Time: 16.32 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+
+        long left = 1;
+
+        long minTime = Long.MAX_VALUE;
+
+        for (int time : times) {
+            minTime = Math.min(minTime, time);
+        }
+
+        long right = minTime * n;
+
+        long answer = right;
+
+        while (left <= right) {
+
+            long mid = (left + right) / 2;
+
+            long people = 0;
+
+            for (int time : times) {
+                people += mid / time;
+
+                if (people >= n) {
+                    break;
+                }
+            }
+
+            if (people >= n) {
+                answer = mid;
+                right = mid - 1;
+
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+}
